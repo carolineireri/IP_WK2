76871a5e60a6fb76bac085c643cd4ad5dc757d47
carolineireri <carolnyamburaireri@gmail.com>
2019-10-27 21:13:57 +0300

    Changed input type from text to number

--- js/script.js
+++ js/script.js
@@ -8,4 +8,19 @@ function akanName() {
     var cc = (yy - 1) / 100 + 1;
     // array for days of the week
     var daysarray = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    var dayofweek = (CC / 4 - 2 * CC - 1 + 5 * YY / 4 + (26 * (MM + 1) / 10) + DD) % 7;
+    console.log(dayofweek);
+
+    var gender = document.querySelector('input[name="gender"]:checked').value;
+    console.log(gender);
+    if (gender === "Female") {
+        // console.log(arrayoffemale);
+        // document.getElementById('demo').innerHTML = femalearray[Math.round(dayofweek)];
+
+    } else {
+        // console.log(arrayofmale);
+        // document.getElementById('demo').innerHTML = malearray[Math.round(dayofweek)];
+
+
+    }
 }
