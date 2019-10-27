b60cb29ef09e6fa955749c99e2c3552353efb912
carolineireri <carolnyamburaireri@gmail.com>
2019-10-27 20:52:55 +0300

    Create arrays on script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function akanName() {
+    var femalearray= ["Akosua", "Aduoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var malearray= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+    var dd= parseInt(document.getElementById("dd").value);
+    var mm=parseInt(document.getElementById("mm").value);
+    var yy=parseInt(document.getElementById("yy").value);
+    var cc = (yy - 1) / 100 + 1;
+    // array for days of the week
+    var daysarray = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+}
